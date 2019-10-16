ee1cc177af662e8ea435046c8aa219af3490e4a1
hsndmr <hasandemir.5802@gmail.com>
2019-10-16 13:24:34 +0300

    ilk hal

--- company/models.py
+++ company/models.py
@@ -10,13 +10,14 @@ class Company(models.Model):
 class Departament(models.Model):
     company = models.ForeignKey(Company,on_delete=models.CASCADE)
     dep_name = models.CharField(max_length=100,verbose_name="departman adı")
-
+    
     def __str__(self):
-        return self.dep_name
+        return '%s %s'%(self.company,self.dep_name)
 
 
 class Staff(models.Model):
-    company = models.ForeignKey(Departament, on_delete=models.CASCADE)
+    dep_name = models.ForeignKey(Departament, on_delete=models.CASCADE,verbose_name="departman adı")
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
     staff_name = models.CharField(max_length=100, verbose_name="Adı-Soyadı")
     saatlik_ücreti = models.IntegerField()
 
