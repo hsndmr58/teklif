f76a44f15d1c1d417f02ead9330f3fce0cdd4978
enes <enes.mutlu2132@gmail.com>
2019-10-16 12:47:15 +0300

    sil

--- company/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Company,Departament,Staff
-# Register your models here.
-admin.site.register(Company)
-admin.site.register(Departament)
-admin.site.register(Staff)
